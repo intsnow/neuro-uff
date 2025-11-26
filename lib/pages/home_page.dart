25d9994e52bf2d0e1205a786a6f89434dab11441
Iago das Neves Trindade da Silva <iagodnts@id.uff.br>
2025-11-26 13:01:15 -0300

    Adicionando funções de navegação aos Icons _ prototipo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart' as fluIcon;
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -18,6 +19,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth  = MediaQuery.of(context).size.width;
+    final String title = super.widget.title;
 
     return Scaffold(
       backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
@@ -82,29 +84,65 @@ class _HomePageState extends State<HomePage> {
             //  'Positioned()' força a barra estar fixa e proporcional, mesmo
             //  com ajuste de tela. Sem o bottom posto, a barra estaria no meio
             //  da tela, na parte superior.
-            bottom: screenHeight * 0.0125,
+            bottom: 0,
+
+            //   Left e Right mantêm o espaçamento das extremidades fixo (no
+            //   caso, em zero, para o 1° e o ultimo Icon estarem fixados sempre
+            //   na ponta).
+
+            //   Junto ao "MainAxisAlignment.spaceEvenly", que força um espaço
+            //  entre cada Icon na children, a barra inferior se manterá em 50%
+            //  da tela.
             left: 0,
             right: 0,
 
             child: SizedBox(
-              height: 50,
+              height: 60,
 
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-                children: [
-                  Icon(fluIcon.FluentIcons.home_32_regular,
-                      color: Color.fromRGBO(124, 156, 191, 1)),
-                  Icon(fluIcon.FluentIcons.book_32_regular,
-                      color: Color.fromRGBO(124, 156, 191, 1)),
-                  Icon(fluIcon.FluentIcons.apps_32_regular,
-                      color: Color.fromRGBO(124, 156, 191, 1)),
-                  Icon(fluIcon.FluentIcons.chat_32_regular,
-                      color: Color.fromRGBO(124, 156, 191, 1)),
-                  Icon(fluIcon.FluentIcons.person_32_regular,
-                      color: Color.fromRGBO(124, 156, 191, 1)),
-                ],
-              ),
+              child:  ColoredBox(
+
+                color: Colors.blueGrey.shade50,
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                  children: [
+                    IconButton(
+                      
+                      padding: EdgeInsets.all(4),
+
+
+                      icon: Icon(
+                          fluIcon.FluentIcons.home_32_regular,
+                          color: Color.fromRGBO(124, 156, 191, 1),
+                      ),
+
+                      onPressed: () {
+                        Navigator.push(
+                            context, MaterialPageRoute(
+                                builder: (context) =>  HomePage(title: title,)
+                            )
+                        );
+
+                      },
+                    ),
+
+
+                    Icon(fluIcon.FluentIcons.book_32_regular,
+                        color: Color.fromRGBO(124, 156, 191, 1)),
+                    Icon(fluIcon.FluentIcons.apps_32_regular,
+                        color: Color.fromRGBO(124, 156, 191, 1)),
+                    Icon(fluIcon.FluentIcons.chat_32_regular,
+                        color: Color.fromRGBO(124, 156, 191, 1)),
+                    Icon(fluIcon.FluentIcons.person_32_regular,
+                        color: Color.fromRGBO(124, 156, 191, 1)),
+                  ],
+                ),
+
+
+              )
+
             ),
           ),
 
